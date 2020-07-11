13f8b39b35f988aa867a4b697b90e994ee6a20c3
AlexeyStitcg <ljrevtyn12!>
2020-07-11 08:04:23 +0500

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,57 +1,26 @@
-import React, {useState} from 'react';
-
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-    const [isOpenCreateTaskFOrm, setIsOpenCreateTaskForm] = useState(false)
-const [isActiveButtonTask, setisActiveButtonTask] = useState(false)
-
-    const openCreateTaskForm = () => {
-        setIsOpenCreateTaskForm(true)
-    }
-
-    const onTaskChange = (e) => {
-        if(e.target.value.length >= 3) {
-setisActiveButtonTask(true)
-        } else {
-            setisActiveButtonTask(false)
-        }
-    }
-
-    return (
-        <div>
-            <div className="container">
-                <h1>Kanban</h1>
-
-                {!isOpenCreateTaskFOrm && <button type="button" className="btn btn-primary" onClick={openCreateTaskForm}>Create Task</button>}
-                {isOpenCreateTaskFOrm &&
-                <form>
-                    <div className="form-group">
-                        <label htmlFor="exampleInputEmail1">Task</label>
-                        <input type="text" className="form-control" onChange={onTaskChange}/>
-                    </div>
-                    <button type="submit" className="btn btn-primary" disabled={!isActiveButtonTask}>Submit</button>
-                </form>
-                }
-                <div className="row">
-
-                    <div className="col-sm">
-                        TO DO
-                    </div>
-
-                    <div className="col-sm">
-                        IN PROGRESS
-                    </div>
-
-                    <div className="col-sm">
-                        REVIEW
-                    </div>
-
-                    <div className="col-sm">
-                        DONE
-                    </div>
-
-                </div>
-            </div>
-        </div>
-);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
+export default App;
